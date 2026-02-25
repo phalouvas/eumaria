211cb7e23bacd24ed3e99089a4ff0627be06a158
Panayiotis Halouvas <phalouvas@gmail.com>
2026-02-25 09:12:39 +0000

    feat: initialize start and end dates for new Eumaria Gift Cards

--- eumaria/eumaria/doctype/eumaria_gift_card/eumaria_gift_card.js
+++ eumaria/eumaria/doctype/eumaria_gift_card/eumaria_gift_card.js
@@ -2,6 +2,22 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Eumaria Gift Card", {
+	onload(frm) {
+		if (!frm.is_new()) {
+			return;
+		}
+
+		const today = frappe.datetime.get_today();
+
+		if (!frm.doc.starts_on) {
+			frm.set_value("starts_on", today);
+		}
+
+		if (!frm.doc.ends_on) {
+			frm.set_value("ends_on", frappe.datetime.add_months(today, 6));
+		}
+	},
+
 	refresh(frm) {
 		// Add button to open linked Payment Entry
 		if (frm.doc.payment_entry) {
